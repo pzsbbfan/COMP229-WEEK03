30e8e6a9b579a1e573a181be2c58fa916e61fccd
Lucas Fan <lucasfan1995@gmail.com>
2022-10-01 03:44:27 -0400

    Adding contents to all the partials and main pages

--- routes/index.js
+++ routes/index.js
@@ -16,12 +16,6 @@ router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About'});
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
-});
-
-
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services'});
@@ -32,6 +26,11 @@ router.get('/services', function(req, res, next) {
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
   res.render('index', { title: 'Contact'});
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects'});
 });
 
 
